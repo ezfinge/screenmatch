b4f57799e3d155fb8754aab1cfb4875e42a3503a
ezfinge <mirciobrandao@gmail.com>
2025-12-23 19:57:47 -0300

    implementação simples

--- src/Principal/PrincipalComBusca.java
+++ src/Principal/PrincipalComBusca.java
@@ -8,16 +8,26 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.Optional;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://www.omdbapi.com/?t=matrix&apikey=8b0601ee"))
-                //fazendo requisição..
-                .build();
+
+        Scanner leitura = new Scanner(System.in);
+
+        System.out.printf("qual o filme?");
+        var busca = leitura.nextLine();
+
+        String endereco = "http://www.omdbapi.com/?t="+busca+"&apikey=8b0601ee";
+
         //https://docs.oracle.com/en/java/javase/17/docs/api/java.net.http/java/net/http/HttpRequest.html
         //http request java doc
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(endereco))
+                //fazendo requisição..
+                .build();
+
 
 
         // http responde -java doc
@@ -26,6 +36,7 @@ public class PrincipalComBusca {
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
 
+        System.out.println(response.body());
 
     }//fim main
 }//fim classe
